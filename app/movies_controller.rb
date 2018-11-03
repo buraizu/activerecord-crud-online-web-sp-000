570a3ba1a93b4b19af0a1bda65f800ae59bf42de
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-03 21:41:19 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,7 +20,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  # movie =
+  movie = Movie.create(attributes)
 end
 
 def can_be_created_in_a_block(args = __)
@@ -28,9 +28,9 @@ def can_be_created_in_a_block(args = __)
   # title == "Home Alone"
   # release_date == 1990
 
-  # Movie.create do |m|
-  #   __
-  #end
+  Movie.create do |m|
+    __
+  end
 end
 
 def can_get_the_first_item_in_the_database
